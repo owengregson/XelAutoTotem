041cbca63a26ea7d6a0303dd3f70147aaea27f62
owengregson <owen@owen.lol>
2024-08-09 23:14:58 -0700

    wrong vernumber

--- src/main/java/me/vexmc/xelautototem/client/XelAutoTotem.java
+++ src/main/java/me/vexmc/xelautototem/client/XelAutoTotem.java
@@ -13,7 +13,7 @@ import java.util.concurrent.CompletableFuture;
 @Environment(EnvType.CLIENT)
 public class XelAutoTotem implements ClientModInitializer {
 	public static final Logger LOGGER = LoggerFactory.getLogger("XelAutoTotem");
-	public static final String VERSION = "2.0";
+	public static final String VERSION = "1.0";
 
 	@Override
 	public void onInitializeClient() {
